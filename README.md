d74686584c6010070a0a121bd006b85fbd90e289
shaammiru <msyamil.hamami@gmail.com>
2025-09-02 14:16:45 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+NodeJs Version: v22.17.0
+NPM Version: 10.9.2
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import coap from "coap";
 
-// bikin server
 const server = coap.createServer();
 
-// handler request
 server.on("request", (req, res) => {
   console.log("📩 Request received:", req.url, req.payload?.toString());
 
@@ -12,12 +10,11 @@ server.on("request", (req, res) => {
   } else if (req.method === "POST") {
     res.end(`Received POST with payload: ${req.payload.toString()}`);
   } else {
-    res.code = "4.05"; // Method Not Allowed
+    res.code = "4.05";
     res.end();
   }
 });
 
-// listen di port default CoAP (5683)
 server.listen(() => {
   console.log("✅ CoAP server listening on port 5683");
 });
